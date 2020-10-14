2049b966f444d96bda32f1c1a47c64b5a5c87ccf
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2020-10-15 00:52:22 +0300

    fix:last task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,10 +1,9 @@
 
-<<<<<<< HEAD
 class VigenereCipheringMachine {
   encrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
+  }
   decrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
@@ -12,5 +11,3 @@ class VigenereCipheringMachine {
 }
 
 module.exports = VigenereCipheringMachine;
-=======
->>>>>>> 820aa615f065fd9cd263b7c67fdff0119a1abb0d
